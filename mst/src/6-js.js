e9071e0e6dd541d3802db13eb228b72c46e3fa73
春雷-李春雷 <chunlei.lcl@naturobot.com>
2023-02-14 16:36:43 +0800

    j

---
+++ mst/src/6-js.js
@@ -0,0 +1,2 @@
+Object.prototype.toString.call([1, 2])
+;[1, 2].toString()
--- mst/src/6-js.ts
+++
@@ -1,8 +0,0 @@
-Object.prototype.toString.call([1, 2]);
-
-delete Array.prototype.toString;
-
-[1, 2].toString()
-
-export const a = 1
-
