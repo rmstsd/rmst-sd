a443670ca6bebc73ecb36ec7720aac7f396e1be3
春雷-李春雷 <chunlei.lcl@naturobot.com>
2023-04-04 18:57:04 +0800

    zz

---
+++ createCanvas.js
@@ -0,0 +1,19 @@
+const dpr = window.devicePixelRatio || 1
+export function createCanvas(containerWidth, containerHeight) {
+  const canvasElement = document.createElement('canvas')
+  const canvasWidth = containerWidth * dpr
+  const canvasHeight = containerHeight * dpr
+
+  canvasElement.width = canvasWidth
+  canvasElement.height = canvasHeight
+  canvasElement.style.width = '100%'
+  canvasElement.style.height = '100%'
+
+  const ctx = canvasElement.getContext('2d')
+
+  ctx.scale(dpr, dpr)
+  ctx.textBaseline = 'top'
+  ctx.font = `${14}px 微软雅黑`
+
+  return { canvasElement, ctx }
+}
