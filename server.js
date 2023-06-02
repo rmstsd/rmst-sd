346f1667716e5dd7f21855bbf8d25296838aabe7
春雷-李春雷 <chunlei.lcl@naturobot.com>
2023-06-02 20:02:26 +0800

    ss

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const http = require('http')
+const fs = require('fs')
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200
-  res.setHeader('Content-Type', 'text/html')
-  const html = '<html><body><h1>Hello, World!</h1></body></html>'
-  res.end(html)
+  res.setHeader('Content-Type', 'text/js')
+  // 跨域
+  res.setHeader('Access-Control-Allow-Origin', '*')
+
+  const buffer = fs.readFileSync('./esm.js')
+
+  setTimeout(() => {
+    res.end(buffer)
+  }, 2000)
 })
 
-const port = 3008
+const port = 10055
 server.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`)
 })
