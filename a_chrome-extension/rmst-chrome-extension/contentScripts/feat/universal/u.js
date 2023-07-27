c03a89406fe1a049506a3e72a4dd839c116808ee
春雷-李春雷 <chunlei.lcl@naturobot.com>
2023-07-27 11:00:21 +0800

    bug

--- a_chrome-extension/rmst-chrome-extension/contentScripts/feat/universal/u.js
+++ a_chrome-extension/rmst-chrome-extension/contentScripts/feat/universal/u.js
@@ -3,9 +3,14 @@ document.body.classList.add('u')
 // 防止 QQ 浏览器 按退格键
 document.addEventListener('keydown', evt => {
   if (evt.key === 'Backspace') {
-    if (evt.target.tagName === 'INPUT' || evt.target.tagName === 'TEXTAREA') {
+    if (
+      evt.target.tagName === 'INPUT' ||
+      evt.target.tagName === 'TEXTAREA' ||
+      evt.target.getAttribute('contenteditable') === 'true'
+    ) {
       return
     }
+
     evt.preventDefault()
   }
 })
