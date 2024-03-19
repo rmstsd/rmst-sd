d8683e679fdf64d8088de9d837070092558a92d8
rmst <2864617610@qq.com>
2024-03-19 22:32:15 +0800

    a

---
+++ interview/js/qgd/a==1.js
@@ -0,0 +1,70 @@
+;(() => {
+  let i = 1
+  with ({
+    get a() {
+      return i++
+    }
+  }) {
+    if (a == 1 && a == 2 && a == 3) {
+      console.log('hello medium')
+    }
+  }
+})()
+
+//
+;(() => {
+  let i = 1
+
+  let a = {
+    valueOf() {
+      return i++
+    }
+  }
+
+  if (a == 1 && a == 2 && a == 3) {
+    console.log('hello medium')
+  }
+})()
+
+//
+;(() => {
+  const a = {
+    i: 1,
+    [Symbol.toPrimitive]() {
+      return this.i++
+    }
+  }
+
+  if (a == 1 && a == 2 && a == 3) {
+    console.log('hello medium') // hello medium
+  }
+})()
+
+//
+;(() => {
+  let a = new Proxy(
+    { i: 1 },
+    {
+      get(target) {
+        return () => target.i++
+      }
+    }
+  )
+
+  if (a == 1 && a == 2 && a == 3) {
+    console.log('hello medium') // hello medium
+  }
+})()
+
+console.log('---------------')
+
+let uu = 10
+const aoo = {
+  get bv() {
+    uu += 10
+    return uu
+  }
+}
+
+console.log(aoo.bv)
+console.log(aoo)
