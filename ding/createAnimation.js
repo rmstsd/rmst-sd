f7d7282dff1cc61d828de4e13b8d169a9ec0152e
rmst <2864617610@qq.com>
2024-04-24 12:31:40 +0800

    asdasd

--- ding/createAnimation.js
+++ ding/createAnimation.js
@@ -1,13 +1,35 @@
-function createAnimation(scrollStart, scrollEnd, valueStart, valueEnd) {
+function createAnimation(scrollStart, scrollEnd, valueStart, valueEnd, halfMode = false) {
+  const third_half = (scrollEnd - scrollStart) / 3
+
   return function (scroll) {
-    if (scroll <= scrollStart) {
-      return valueStart
-    }
+    if (halfMode) {
+      if (scroll <= scrollStart) {
+        return valueStart
+      }
 
-    if (scroll >= scrollEnd) {
-      return valueEnd
-    }
+      if (scroll >= scrollEnd) {
+        return valueStart
+      }
 
-    return valueStart + (valueEnd - valueStart) * ((scroll - scrollStart) / (scrollEnd - scrollStart))
+      if (scrollStart + third_half < scroll && scroll < scrollEnd - third_half) {
+        return valueEnd
+      }
+
+      if (scroll <= scrollStart + third_half) {
+        return valueStart + (valueEnd - valueStart) * ((scroll - scrollStart) / third_half)
+      }
+
+      return valueEnd - ((valueEnd - valueStart) * (scroll - (scrollEnd - third_half))) / third_half
+    } else {
+      if (scroll <= scrollStart) {
+        return valueStart
+      }
+
+      if (scroll >= scrollEnd) {
+        return valueEnd
+      }
+
+      return valueStart + (valueEnd - valueStart) * ((scroll - scrollStart) / (scrollEnd - scrollStart))
+    }
   }
 }
