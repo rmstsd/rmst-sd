631e455f1ce131a4a38d55e42d58d405080cbb83
rmst <rmstsd@qq.com>
2024-05-13 18:24:25 +0800

    gs

--- 数据格式.ts
+++ 数据格式.ts
@@ -11,60 +11,62 @@
 // 1. buff 都是 json
 
 // 注册事件
-// 游戏demo功能,游戏demo日志列表
+// 游戏demo功能, 游戏demo日志列表
+
 const register = {
   toolBars: [
     {
       key: 'isShowCollisionBox',
       label: '碰撞盒显示',
-      defaultValue: true, // 碰撞盒显示
+      defaultValue: true, // 布尔类型
       renderType: 'switch'
     },
     {
+      key: 'isShowSkillPath',
       label: '显示技能体路径',
-      renderType: 'switch',
-      isShowSkillPath: true // 显示技能体路径
+      defaultValue: true, // 布尔类型
+      renderType: 'switch'
     },
     {
+      key: 'isShowAOESkillHarmAreaHint',
       label: 'AOE技能伤害范围指示',
-      renderType: 'switch',
-      isShowAOESkillHarmAreaHint: true //
+      defaultValue: true, // 布尔类型
+      renderType: 'switch'
     }
-  ],
-  logList: []
+  ]
 }
+
+// logList: []
 
 // event(key, value)
 // event(onplay, value)
 
-const data = {
-  // 战斗单位
-  combatUnitList: [
-    {
-      camp: '敌方', // 敌方/友方
-      unit: '法师(1001)', // 单位
-      unitCategory: '职业 | 宠物 | 怪物', // 单位类型
-      // 属性
-      property: [
-        {
-          propertyName: '最大生命',
-          propertyValue: 111
-        }
-      ],
-      // 技能
-      skill: [
-        {
-          skillName: '火球术',
-          skillSort: 1,
-          技能详情数据: {} // 技能详情数据 是 json  // 需要效果嵌套
-        }
-      ],
-      buff: [
-        {
-          buffValue: '攻击上升23%',
-          buff数据: {} // buff数据 是 json
-        }
-      ]
-    }
-  ]
-}
+// 战斗单位列表
+const combatUnitList = [
+  {
+    camp: '敌方', // 敌方/友方
+    unit: '法师(1001)', // 单位
+    unitCategory: '职业', // 单位类型: 职业 | 宠物 | 怪物
+    // 属性
+    property: [
+      {
+        propertyName: '最大生命',
+        propertyValue: 111
+      }
+    ],
+    // 技能
+    skill: [
+      {
+        skillName: '火球术',
+        skillSort: 1,
+        技能详情数据: {} // 技能详情数据 是 json  // 效果嵌套
+      }
+    ],
+    buff: [
+      {
+        buffValue: '攻击上升23%',
+        buff数据: {} // buff数据 是 json
+      }
+    ]
+  }
+]
