22dc98c341eee5fea966e627135f49848916260d
rmst <rmstsd@qq.com>
2024-05-31 10:33:17 +0800

    dasdas

--- 蓝图.ts
+++ 蓝图.ts
@@ -1,65 +1,32 @@
-export interface Node {
-  id: string
-  title: string
+interface Node {
+  acceptType: null | string | string[]
 
-  acceptType?: null | string | string[] // 接受的类型
+  properties: {
+    userName: {
+      contactIds: null | string | string[]
 
-  fieldList: Field[]
-}
+      title: string
+      component: 'Input'
+      componentProps?: {}
+      initialValue?: any
 
-interface Field {
-  key: string
-  label: string
-  value: string | Field[]
-  renderType: 'text' | 'input' | 'radio' | 'select'
+      enum?: { label: string; value: string | number }[] // component: 'Select' 才有用
+    }
+    email: {
+      contactIds: null | string | string[]
 
-  slot?: Field[] // 描述 key 的额外信息
-
-  contactNodeIds?: null | string | string[]
-  emitType: null | string | string[] // 可以连接的类型
-
-  interaction?: { click?: boolean }
-
-  ext?: any
-}
-
-const data: Node[] = [
-  {
-    id: 'asd',
-    title: '',
-    fieldList: [
-      {
-        key: 'range',
-        label: '施法范围',
-        value: '1200',
-        renderType: 'input',
-        contactNodeIds: 'zxc',
-        emitType: 'effect'
+      title: string
+      component: 'Input'
+      componentProps?: {}
+      initialValue?: any
+    }
+    layout: {
+      type: 'void'
+      component: 'FormLayout'
+      componentProps?: {
+        layout: 'vertical'
       }
-    ]
-  },
-  {
-    id: 'zxc',
-    title: '',
-    acceptType: 'effect',
-    fieldList: [
-      {
-        key: 'range',
-        label: '施法范围',
-        value: [
-          {
-            key: 'time',
-            label: '施放时间',
-            value: '0.1',
-            renderType: 'input',
-            contactNodeIds: null,
-            emitType: 'effect'
-          }
-        ],
-        renderType: 'input',
-        contactNodeIds: null,
-        emitType: 'effect'
-      }
-    ]
+      properties: {}
+    }
   }
-]
+}
