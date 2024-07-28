3283cf9b364728878451604d569f0614cb7f3c84
rmst <rmstsd@qq.com>
2024-07-28 19:38:58 +0800

    sev

--- a_chrome-extension/废弃_context-menus/popup/popup.js
+++
@@ -1,9 +0,0 @@
-onload = () => {
-  document.querySelector('button').onclick = aa
-}
-
-async function aa() {
-  const [currTab] = await chrome.tabs.query({ active: true })
-
-  await chrome.tabs.sendMessage(currTab.id, { evt: 'evt_to-lowercase' })
-}
