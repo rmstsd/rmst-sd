8e6efe8b08cebc2c215e33c9ee8422d69c124602
rmst <rmstsd@qq.com>
2024-10-15 15:58:58 +0800

    code

--- z-dt/debounce.js
+++ z-dt/debounce.js
@@ -7,23 +7,24 @@ const delay = 2000
   const debouncePanel = document.querySelector('.debounce-panel-leading')
   let debounceCount = 0
   debouncePanel.textContent = debounceCount
-
   function addCount() {
     debounceCount++
     debouncePanel.textContent = debounceCount
   }
 
-  let timer
-  debouncePanel.addEventListener('pointermove', () => {
-    if (!timer) {
-      addCount()
-    }
+  {
+    let timer
+    debouncePanel.addEventListener('pointermove', () => {
+      if (!timer) {
+        addCount()
+      }
 
-    clearTimeout(timer)
-    timer = setTimeout(() => {
-      timer = null
-    }, delay)
-  })
+      clearTimeout(timer)
+      timer = setTimeout(() => {
+        timer = null
+      }, delay)
+    })
+  }
 }
 
 {
@@ -31,18 +32,19 @@ const delay = 2000
   const debouncePanel = document.querySelector('.debounce-panel-trailing')
   let debounceCount = 0
   debouncePanel.textContent = debounceCount
-
   function addCount() {
     debounceCount++
     debouncePanel.textContent = debounceCount
   }
 
-  let timer
-  debouncePanel.addEventListener('pointermove', () => {
-    clearTimeout(timer)
+  {
+    let timer
+    debouncePanel.addEventListener('pointermove', () => {
+      clearTimeout(timer)
 
-    timer = setTimeout(() => {
-      addCount()
-    }, delay)
-  })
+      timer = setTimeout(() => {
+        addCount()
+      }, delay)
+    })
+  }
 }
--- z-dt/throttle.js
+++ z-dt/throttle.js
@@ -7,22 +7,23 @@ const delay = 2000
   const throttlePanel = document.querySelector('.throttle-panel-leading')
   let throttleCount = 0
   throttlePanel.textContent = throttleCount
-
   function addCount() {
     throttleCount++
     throttlePanel.textContent = throttleCount
   }
 
-  let lastTime = 0
-  throttlePanel.addEventListener('pointermove', () => {
-    const currentTime = Date.now()
+  {
+    let lastTime = 0
+    throttlePanel.addEventListener('pointermove', () => {
+      const currentTime = Date.now()
 
-    if (currentTime - lastTime >= delay) {
-      lastTime = currentTime
+      if (currentTime - lastTime >= delay) {
+        lastTime = currentTime
 
-      addCount()
-    }
-  })
+        addCount()
+      }
+    })
+  }
 }
 
 {
@@ -30,25 +31,26 @@ const delay = 2000
   const throttlePanel = document.querySelector('.throttle-panel-trailing')
   let throttleCount = 0
   throttlePanel.textContent = throttleCount
-
   function addCount() {
     throttleCount++
     throttlePanel.textContent = throttleCount
   }
 
-  let isHasTask = false
-  throttlePanel.addEventListener('pointermove', () => {
-    if (isHasTask) {
-      return
-    }
+  {
+    let isHasTask = false
+    throttlePanel.addEventListener('pointermove', () => {
+      if (isHasTask) {
+        return
+      }
 
-    isHasTask = true
-    setTimeout(() => {
-      isHasTask = false
+      isHasTask = true
+      setTimeout(() => {
+        isHasTask = false
 
-      addCount()
-    }, delay)
-  })
+        addCount()
+      }, delay)
+    })
+  }
 }
 
 {
@@ -56,34 +58,35 @@ const delay = 2000
   const throttlePanel = document.querySelector('.throttle-panel-leading-trailing')
   let throttleCount = 0
   throttlePanel.textContent = throttleCount
-
   function addCount() {
     throttleCount++
     throttlePanel.textContent = throttleCount
   }
 
-  let lastTime = 0
-  let timer = null
+  {
+    let lastTime = 0
+    let timer = null
 
-  throttlePanel.addEventListener('pointermove', () => {
-    const currentTime = Date.now()
-    const remain = delay - (currentTime - lastTime)
+    throttlePanel.addEventListener('pointermove', () => {
+      const currentTime = Date.now()
+      const remain = delay - (currentTime - lastTime)
 
-    if (remain <= 0) {
-      lastTime = currentTime
-      if (timer) {
-        clearTimeout(timer)
-        timer = null
-      }
-
-      addCount()
-    } else if (!timer) {
-      timer = setTimeout(() => {
-        lastTime = 0
-        timer = null
+      if (remain <= 0) {
+        lastTime = currentTime
+        if (timer) {
+          clearTimeout(timer)
+          timer = null
+        }
 
         addCount()
-      }, remain)
-    }
-  })
+      } else if (!timer) {
+        timer = setTimeout(() => {
+          lastTime = 0
+          timer = null
+
+          addCount()
+        }, remain)
+      }
+    })
+  }
 }
