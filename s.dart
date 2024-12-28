d3ade6f1a5434e73035a3534e0cf518334c831f4
rmst <rmstsd@qq.com>
2024-12-28 13:25:40 +0800

    yuu

---
+++ s.dart
@@ -0,0 +1,29 @@
+import 'dart:math';
+
+void main() {
+  var name = "John";
+  var age = 25;
+
+  const ui = 1;
+
+  print(Random());
+
+  age = 30;
+
+  var ans = [
+    1,
+    2,
+    3,
+    4,
+  ];
+
+  var aa = {"name": name, age: age, ans: ans};
+
+  print(aa);
+
+  asd(age);
+}
+
+void asd(int aa) {
+  print(aa);
+}
