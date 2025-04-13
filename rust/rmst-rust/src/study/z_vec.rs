2c0febfd7270412522e10467e15fea32f38e14e5
rmst <rmstsd@gmail.com>
2025-04-13 21:08:15 +0800

    ddd

--- rust/rmst-rust/src/study/z_vec.rs
+++ rust/rmst-rust/src/study/z_vec.rs
@@ -1,17 +1,62 @@
 pub fn z_vec() {
   let num = 55;
-  let mut list = vec![1, 2, 3, num];
+  let list = vec![1, 2, 3, num];
+  // list.retain(f);
 
-  // let v_squared = list.iter().map(|x: &i32| x * x).collect();
-  // let v_even = list.iter().filter(|x| *x % 2 == 0).collect();
+  // println!("{list:#?}");
+  let filtered_list: Vec<_> = list.iter().filter(|x| **x > 3).collect();
+  // println!("{filtered_list:#?}");
 
-  println!("{num:#?}");
+  let mut c1 = Counter::new(1);
 
-  for item in &mut list {
-    *item += 1; // * 解引用运算符
+  println!("{:#?}", c1.get_number());
+  println!("{:#?}", c1.get_number());
+  println!("{:#?}", c1.get_number());
+
+  // c1.add(2);
+  println!("{:#?}", c1.get_number());
+  println!("{:#?}", c1.get_number());
+
+  // c1.give_up();
+  // println!("{:#?}", cc.get_number());
+
+  let c2 = Counter::new(2);
+
+  let ans = Counter::combine(&c1, &c2);
+  println!("{ans:#?}");
+
+  println!("{:#?}", c1.get_number());
+  println!("{:#?}", c2.get_number());
+}
+
+#[derive(Debug)]
+struct Counter {
+  number: i32,
+}
+
+impl Counter {
+  fn new(number: i32) -> Self {
+    Self { number }
   }
 
-  println!("{list:#?}");
+  // 不可变借用
+  fn get_number(&self) -> i32 {
+    self.number
+  }
 
-  println!("{num:#?}");
+  // 可变借用
+  fn add(&mut self, count: i32) {
+    self.number += count;
+  }
+
+  // moved
+  fn give_up(self) {
+    // println!("{:#?}", self.number);
+  }
+
+  fn combine(c1: &Counter, c2: &Counter) -> Counter {
+    Self {
+      number: c1.number + c2.number,
+    }
+  }
 }
