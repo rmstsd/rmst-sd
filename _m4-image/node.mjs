74df6b7abd421e3126c39d848cfb3e55dd29b0b9
rmst <rmstsd@qq.com>
2025-08-01 15:19:53 +0800

    M4图集

---
+++ _m4-image/node.mjs
@@ -0,0 +1,58 @@
+// @ts-check
+
+import { readdir, writeFile } from 'fs/promises'
+import { join } from 'path'
+
+const baseDir_en_mobile = './M4图集/M4 调度图集_英文/移动端'
+const baseDir_en_pc = './M4图集/M4 调度图集_英文/桌面端'
+
+const baseDir_zh_mobile = './M4图集/M4 调度图集_中文/移动端'
+const baseDir_zh_pc = './M4图集/M4 调度图集_中文/桌面端'
+
+async function getAllImageFiles(dir) {
+  const files = []
+
+  async function scan(currentDir) {
+    const entries = await readdir(currentDir, { withFileTypes: true })
+
+    for (const entry of entries) {
+      const fullPath = join(currentDir, entry.name)
+
+      if (entry.isDirectory()) {
+        await scan(fullPath)
+      } else if (entry.isFile() && /\.(jpg|jpeg|png|svg)$/i.test(entry.name)) {
+        files.push({
+          name: entry.name,
+          path: fullPath.replace(/\\/g, '/')
+        })
+      }
+    }
+  }
+
+  await scan(dir)
+  return files
+}
+
+;(async () => {
+  const images_zh_pc = await getAllImageFiles(baseDir_zh_pc)
+  const images_zh_mobile = await getAllImageFiles(baseDir_zh_mobile)
+
+  const images_en_pc = await getAllImageFiles(baseDir_en_pc)
+  const images_en_mobile = await getAllImageFiles(baseDir_en_mobile)
+
+  const images_list = [
+    {
+      title: '中文',
+      lang: 'zh',
+      image: { pc: images_zh_pc, mobile: images_zh_mobile }
+    },
+    {
+      title: '英文',
+      lang: 'en',
+      image: { pc: images_en_pc, mobile: images_en_mobile }
+    }
+  ]
+
+  const json = JSON.stringify(images_list, null, 2)
+  await writeFile('./data.js', `const images_list = ${json}`)
+})()
--- rust/rmst-rust/src/main.rs
+++ rust/rmst-rust/src/main.rs
@@ -7,5 +7,22 @@ use std::{fs, io};
 use crate::fs_op::list_name::list_names;
 
 fn main() {
-  list_names();
+  // list_names();
+
+  dbg!(&0);
+  let ans = aa();
+
+  if let Ok(ans) = ans {
+    dbg!(&ans);
+  } else {
+    dbg!(&"--err");
+  }
+
+  dbg!(&1);
+}
+
+fn aa() -> Result<(), i32> {
+  let a: String = None.ok_or::<i32>(11)?;
+
+  Ok(())
 }
