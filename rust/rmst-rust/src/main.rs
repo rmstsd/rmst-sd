2634b378b27d5350d84a6b228afe2acc4d537198
rmst <rmstsd@qq.com>
2025-08-01 18:14:32 +0800

    豆包 result  的 and()

--- rust/rmst-rust/src/main.rs
+++ rust/rmst-rust/src/main.rs
@@ -2,27 +2,38 @@
 
 mod fs_op;
 
-use std::{fs, io};
+use std::{
+  fs::{self, File},
+  io,
+};
 
 use crate::fs_op::list_name::list_names;
 
 fn main() {
-  // list_names();
+  // dbg!(&0);
+  // let ans = aa().expect("aa");
+  // dbg!(&ans);
 
-  dbg!(&0);
-  let ans = aa();
-
-  if let Ok(ans) = ans {
-    dbg!(&ans);
-  } else {
-    dbg!(&"--err");
-  }
-
-  dbg!(&1);
+  let x: Result<i32, ()> = Ok(2);
 }
 
-fn aa() -> Result<(), i32> {
-  let a: String = None.ok_or::<i32>(11)?;
+fn aa() -> Result<(), io::Error> {
+  let aa: Result<File, io::Error> = File::open("");
+
+  let aa = aa.is_err_and(|x| {
+    dbg!(&x);
+
+    let code = x.raw_os_error().unwrap();
+    dbg!(&code);
+    let kk = x.kind();
+    dbg!(&kk);
+    println!("-- {}", x.to_string());
+
+    true
+  });
+  dbg!(&aa);
+
+  dbg!(&2);
 
   Ok(())
 }
