18ee334800b5ab2d1314a5136451d573b78e70bd
rmst <rmstsd@qq.com>
2025-08-08 16:00:52 +0800

    up

--- node/index.mjs
+++ node/index.mjs
@@ -8,4 +8,11 @@ import path from 'node:path'
 
 // execSync('wt -d E:\\trick-ui --title 哈哈哈 --suppressApplicationTitle')
 
-console.log(path.basename(`e:\\rmst-sd\\zz`))
+// console.log(path.basename(`e:\\rmst-sd\\zz`))
+
+import { ip, ipv6, mac } from 'address'
+
+// default interface 'eth' on linux, 'en' on osx.
+const ip2 = ip() // '192.168.0.2'
+
+console.log(ip2)
